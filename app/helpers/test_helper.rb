f677a66f85345cf4cc2d60a46605f24e6bf66bf5
unknown <sharonkessio6@gmail.com>
2023-02-20 14:39:11 +0300

    fix rubocop erros

--- app/helpers/test_helper.rb
+++ app/helpers/test_helper.rb
@@ -1,2 +1,4 @@
+# rubocop: disable Layout/EndOfLine
 module TestHelper
 end
+# rubocop: enable Layout/EndOfLine
--- test/controllers/test_controller_test.rb
+++ test/controllers/test_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class TestControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get test_index_url
     assert_response :success
   end
