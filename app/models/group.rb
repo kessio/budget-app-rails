b605c838e84dcf1f3c954395db964bac1af04a81
unknown <sharonkessio6@gmail.com>
2023-02-22 18:01:40 +0300

    associations and validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :group_expenses, dependent: :destroy
+    has_many :expenses, through: :group_expenses
+
+    validates :name, :icon presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'test/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # root "home#index"
 end
