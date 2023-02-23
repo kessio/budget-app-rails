e99535391d934f6fff4c39dd25ad6c4b39b6f7a5
unknown <sharonkessio6@gmail.com>
2023-02-23 11:01:31 +0300

    group controller functionality working

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -65,6 +65,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:name, :icon)
+      params.require(:group).permit(:name, :icon).merge(author_id: current_user.id)
     end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,6 @@ class Group < ApplicationRecord
     has_many :group_expenses, dependent: :destroy
     has_many :expenses, through: :group_expenses
 
-    validates :name, :icon presence: true
+    validates :name, presence: true
+    validates :icon, presence: true
 end
