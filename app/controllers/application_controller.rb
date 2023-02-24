5fa6392fcdffad3762e66b826b2541c8b650343c
unknown <sharonkessio6@gmail.com>
2023-02-24 16:46:44 +0300

    sign up design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,12 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
-        devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:name, :email, :password, :password_confirmation, :role)
       end
+  
+      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+        user_params.permit(:email, :password, :remember_me)
+      end
+    end
 end
