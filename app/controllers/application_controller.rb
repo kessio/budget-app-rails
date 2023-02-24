aabb1678f405801ce6dd32ba77461303a0668111
unknown <sharonkessio6@gmail.com>
2023-02-24 17:40:50 +0300

    register done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,12 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :update_allowed_parameters, if: :devise_controller?
 
-    private
-    def authenticate_user!
-      if user_signed_in?
-        super
-      else
-        redirect_to home_index_path, notice: "Please Login to view that page!" if request.original_fullpath != home_index_path
-      end
-    end
+    protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-        user_params.permit(:name, :email, :password, :password_confirmation, :role)
-      end
-  
-      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-        user_params.permit(:email, :password, :remember_me)
-      end
-    end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: 'groups#index'
 
-  resources :categories, controller: 'groups', as: 'groups'  do
+resources :categories, controller: 'groups', as: 'groups'  do
     resources :transactions, controller: 'expenses', as: 'expenses'
   end
  
   devise_for :users
+
+  devise_scope :users do
+    authenticated :users do
+      root 'groups#index', as: :authenticated_user
+    end
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_user
+    end
+  end
 
 end
